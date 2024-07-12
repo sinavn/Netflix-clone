0e6b0460bdf20f49838573c1ac17a0a19d264d94
SinaVn <vosogh858sina@gmail.com>
2024-07-12 16:01:55 +0330

    Initial Commit

---
+++ Netflix clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix clone
+//
+//  Created by Sina Vosough Nia on 4/22/1403 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
