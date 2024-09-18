3af269a7e7ff249ce59f565e81d6aeec4c878c52
SinaVn <vosogh858sina@gmail.com>
2024-09-18 19:13:20 +0330

    add missing files

---
+++ Netflix clone/Utilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Netflix clone
+//
+//  Created by Sina Vosough Nia on 5/9/1403 AP.
+//
+
+import Foundation
+
+struct Constants {
+    static let TMDB_API_Key = "0f04e0e2babb9c8e7c6dcc9c4256595e"
+    static let YT_API_Key = "AIzaSyDXnswrIyqlB_1q6nOti9UrdPIu3Ogdr08"
+    static let TMDB_BaseURL = "https://api.themoviedb.org/3/"
+    static let YT_BaseURL = "https://www.googleapis.com/youtube/v3/search?"
+}
